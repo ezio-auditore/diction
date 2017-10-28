45e02907f2785b2d1f32fc6507fc9c1eb4045e5b
Kaustav Majumder <pupunmajumder@gmail.com>
2017-10-28 14:45:01 +0530

    fixed layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ViewSpeechComponent } from './view-speech/view-speech.component';
+import { SubmitSpeechComponent } from './submit-speech/submit-speech.component';
+import { SearchSpeechComponent } from './search-speech/search-speech.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewSpeechComponent,
+    SubmitSpeechComponent,
+    SearchSpeechComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view-speech/view-speech.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-speech',
+  templateUrl: './view-speech.component.html',
+  styleUrls: ['./view-speech.component.css']
+})
+export class ViewSpeechComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
